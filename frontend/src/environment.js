c1490b8ce53df2c6816757bcff19d45a481f1dfd
Ramya P <rpuliadi@gmail.com>
2024-02-10 18:20:58 +0000

    Corrected fastAPIUrl

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    fastAPIUrl: 'https://rpuliadi-llmfastapi-wu1ft7xcu20.ws-us108.gitpod.io'
+    fastAPIUrl: 'https://8000-rpuliadi-llmfastapi-wu1ft7xcu20.ws-us108.gitpod.io/get_gemini_completion'
   };
